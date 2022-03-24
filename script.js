07852a9e2a49db55c9caacf5d5aace6757748d50
Robert Hassenmayer <rob@localhost.localdomain>
2022-03-24 03:01:02 -0400

    base functionality added

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+const linkContainer = document.querySelector(".link-container");
 const pictureElement = document.querySelector(".picture");
 const myName = document.querySelector(".my-name");
 const infoText = document.querySelector(".info-text");
+const link1 = document.querySelector(".project-folder-link-1");
+const link2 = document.querySelector(".project-folder-link-2");
+const link3 = document.querySelector(".project-folder-link-3");
+const infoFlex = document.querySelector(".information-flex")
+var images = {
+    img0: "863992.png",
+    img1: "717608.png",
+    img2: "996677.jpg",
+    img3: "1067764.png"
+};
 
-function changeInfoBox(imgElement, titleElement, infoElement){
-    imgElement.src = "1067764.png";
-    titleElement.style.border = "5px solid red";
-    infoElement.style.border = "5px solid red";
-}
-changeInfoBox(pictureElement, myName, infoText)
+linkContainer.addEventListener("mouseover", e => {
+    pictureElement.src = images.img0;
+});
 
-//mousover event listener
+infoFlex.addEventListener("mouseover", e => {
+    pictureElement.src = images.img0;
+});
+
+link1.addEventListener("mouseover", e => {
+    e.stopPropagation();
+    pictureElement.src = images.img1;
+});
+
+link2.addEventListener("mouseover", e => {
+    e.stopPropagation();
+    pictureElement.src = images.img2;
+});
+
+link3.addEventListener("mouseover", e => {
+    e.stopPropagation();
+    pictureElement.src = images.img3;
+});
+
+
